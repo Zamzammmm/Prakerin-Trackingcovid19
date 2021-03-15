840a066ac7be65e3b926c481505c626c9d44bf27
zamzammmm <zamzamzuba2017@gmail.com>
2021-03-15 16:31:51 +0700

    complete crud

--- resources/views/admin/provinsi/show.blade.php
+++ resources/views/admin/provinsi/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.backend')
-
+@section('active')
+<div class="content-header">
+    <div class="container-fluid">
+    <div class="row mb-2">
+        <div class="col-sm-6">
+        <h1 class="m-0">Lihat Provinsi</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+            <li class="breadcrumb-item active">Provinsi</li>
+        </ol>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+@endsection
 @section('content')
 <div class="container mt-3">
     <div class="card">
